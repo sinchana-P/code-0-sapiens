a97e9a4db6593762d4c8d7b03a1bcb9ad401ff64
NandishBS <nandishbs11@gmail.com>
2024-02-28 23:57:38 +0530

    created get and created sql connection

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,19 @@
 import express from "express"
+import mysql from "mysql2"
 
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"mysql",
+    database:""
+})
 const port = 3000
 const app = express()
 
 app.listen(port,()=>{
     console.log("server started at http://localhost:"+port);
+})
+
+app.get("/",(req,res)=>{
+    res.send("HI");
 })
