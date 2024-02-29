4af9482188a53f569529d6f35015c03abd3248af
Sanketh Kumar <sankethkumarbg1995@gmail.com>
2024-02-29 17:44:45 +0530

    Updated sql file

--- sql_file/college_user.sql
+++ sql_file/college_user.sql
@@ -16,6 +16,164 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `attendance`
+--
+
+DROP TABLE IF EXISTS `attendance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `attendance` (
+  `id` varchar(20) NOT NULL,
+  `sub_id` varchar(45) NOT NULL,
+  `date` date DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  PRIMARY KEY (`id`,`sub_id`),
+  KEY `sub_id_idx` (`sub_id`),
+  CONSTRAINT `id` FOREIGN KEY (`id`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `sub_id` FOREIGN KEY (`sub_id`) REFERENCES `subjects` (`sub_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attendance`
+--
+
+LOCK TABLES `attendance` WRITE;
+/*!40000 ALTER TABLE `attendance` DISABLE KEYS */;
+/*!40000 ALTER TABLE `attendance` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chapter`
+--
+
+DROP TABLE IF EXISTS `chapter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chapter` (
+  `chp_id` varchar(10) NOT NULL,
+  `chapter_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`chp_id`),
+  UNIQUE KEY `chapter_name_UNIQUE` (`chapter_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chapter`
+--
+
+LOCK TABLES `chapter` WRITE;
+/*!40000 ALTER TABLE `chapter` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chapter` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `modules`
+--
+
+DROP TABLE IF EXISTS `modules`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `modules` (
+  `mod_id` varchar(10) NOT NULL,
+  `module_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`mod_id`),
+  UNIQUE KEY `module_name_UNIQUE` (`module_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `modules`
+--
+
+LOCK TABLES `modules` WRITE;
+/*!40000 ALTER TABLE `modules` DISABLE KEYS */;
+/*!40000 ALTER TABLE `modules` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notes`
+--
+
+DROP TABLE IF EXISTS `notes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notes` (
+  `sem_id` varchar(10) NOT NULL,
+  `sub_id` varchar(10) NOT NULL,
+  `mod_id` varchar(10) NOT NULL,
+  `chp_id` varchar(10) NOT NULL,
+  `pdf` varchar(10) DEFAULT NULL,
+  `link` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`sem_id`,`sub_id`,`mod_id`,`chp_id`),
+  KEY `sub_id2_idx` (`sub_id`),
+  KEY `mod_id_idx` (`mod_id`),
+  KEY `chp_id_idx` (`chp_id`),
+  CONSTRAINT `chp_id` FOREIGN KEY (`chp_id`) REFERENCES `chapter` (`chp_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `mod_id` FOREIGN KEY (`mod_id`) REFERENCES `modules` (`mod_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `sem_id` FOREIGN KEY (`sem_id`) REFERENCES `semester` (`sem_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `sub_id2` FOREIGN KEY (`sub_id`) REFERENCES `subjects` (`sub_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notes`
+--
+
+LOCK TABLES `notes` WRITE;
+/*!40000 ALTER TABLE `notes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `semester`
+--
+
+DROP TABLE IF EXISTS `semester`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `semester` (
+  `sem_id` varchar(10) NOT NULL,
+  `semester_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`sem_id`),
+  UNIQUE KEY `semester_name_UNIQUE` (`semester_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `semester`
+--
+
+LOCK TABLES `semester` WRITE;
+/*!40000 ALTER TABLE `semester` DISABLE KEYS */;
+/*!40000 ALTER TABLE `semester` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subjects`
+--
+
+DROP TABLE IF EXISTS `subjects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subjects` (
+  `sub_id` varchar(10) NOT NULL,
+  `subject_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`sub_id`),
+  UNIQUE KEY `subject_name_UNIQUE` (`subject_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subjects`
+--
+
+LOCK TABLES `subjects` WRITE;
+/*!40000 ALTER TABLE `subjects` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subjects` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -27,9 +185,10 @@ CREATE TABLE `user` (
   `NAME` varchar(45) NOT NULL,
   `PASSWORD` varchar(45) NOT NULL,
   `ROLE` varchar(20) NOT NULL,
-  `PHONE` int NOT NULL,
+  `PHONE` bigint NOT NULL,
   PRIMARY KEY (`ID`),
-  UNIQUE KEY `NAME_UNIQUE` (`NAME`)
+  UNIQUE KEY `NAME_UNIQUE` (`NAME`),
+  UNIQUE KEY `PHONE_UNIQUE` (`PHONE`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -39,6 +198,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('COST001','Sanketh','sanketh','teacher',9611076613);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -51,4 +211,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-29 16:38:11
+-- Dump completed on 2024-02-29 17:43:43
