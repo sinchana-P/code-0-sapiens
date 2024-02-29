d16f0612d7fde489aff9580b3df02625380cacb4
NandishBS <nandishbs11@gmail.com>
2024-02-29 16:34:07 +0530

    testing login page

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import mysql from "mysql2"
-
+import bodyParser from "body-parser"
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
@@ -10,10 +10,30 @@ const db = mysql.createConnection({
 const port = 3000
 const app = express()
 
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.listen(port,()=>{
     console.log("server started at http://localhost:"+port);
 })
 
-app.get("/",(req,res)=>{
-    res.send("HI Nandish");
+app.get("/login",(req,res)=>{
+    let role = req.body.role;
+    let userid = req.body.userid;
+    let password = req.body.password;
+
+    sql = ""
+    db.query(sql,(err,result)=>{
+        if(err){
+            res.json({authentication:false})
+        }
+        else{
+            if(result.length == 0){
+                res.json({authentication:false}
+                )
+            }
+            else{
+                res.json({authentication:true})
+            }
+        }
+    })
 })
