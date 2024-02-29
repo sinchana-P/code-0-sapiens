56c9c9f44765b84eda389a49c62a33e19ca27833
Sinchana P <sinchana.p.swarna@gmail.com>
2024-02-29 16:24:03 +0530

    first commit
    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Code-0-Sapiens</h1>
+    </div>
+  );
+}
+
+export default App;
