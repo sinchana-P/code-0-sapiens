ebdc720f2bbf00b92e4137d6ed993433ecb6c6eb
NandishBS <nandishbs11@gmail.com>
2024-03-01 11:21:33 +0530

    lets do it

--- client/src/components/homeScene/Home.jsx
+++ client/src/components/homeScene/Home.jsx
@@ -1,51 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './home.css'; 
 
 const Home = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const showSlide = (index) => {
-    const slider = document.getElementById('image-slider');
-    const sliderWidth = document.querySelector('.slider-image').clientWidth;
-    const newPosition = -index * sliderWidth;
-    slider.style.transform = `translateX(${newPosition}px)`;
-    setCurrentIndex(index);
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
   };
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % 3); // Change 3 to the total number of images
-    showSlide(currentIndex);
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + 3) % 3); // Change 3 to the total number of images
-    showSlide(currentIndex);
-  };
-
-  useEffect(() => {
-    const slideInterval = setInterval(nextSlide, 3000); // Change 3000 to the desired interval
-
-    return () => {
-      clearInterval(slideInterval);
-    };
-  }, [currentIndex]);
 
   return (
-    <>
-      <header>
-        {/* Your header content here */}
-      </header>
-      <div className="slide-background">
-        <div id="slider-container">
-          <div id="image-slider">
-            <img src="image1.jpg" alt="Image 1" className="slider-image" />
-            <img src="image2.jpg" alt="Image 2" className="slider-image" />
-            <img src="image3.jpg" alt="Image 3" className="slider-image" />
-          </div>
-          <button id="prev-btn" className="slider-btn" onClick={prevSlide}>❮</button>
-          <button id="next-btn" className="slider-btn" onClick={nextSlide}>❯</button>
+    <div className="slider-container">
+      <div className="background-image"></div>
+      <Slider {...sliderSettings}>
+        <div>
+          <img src="./images/image1.jpg" alt="Image 1" className="slider-image" />
         </div>
-      </div>
-    </>
+        <div>
+          <img src="./images/image1.jpg" alt="Image 2" className="slider-image" />
+        </div>
+        <div>
+          <img src="./images/image1.jpg" alt="Image 3" className="slider-image" />
+        </div>
+      </Slider>
+    </div>
   );
 };
 
